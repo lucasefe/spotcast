5e6dbee2c9f9f8858bdde62d7955433723b372d0
Lucas Florio <lucasefe@gmail.com>
2020-03-25 12:02:30 -0300

    server no longer a class, also, embrace express

--- src/server/index.ts
+++ src/server/index.ts
@@ -6,74 +6,73 @@ import { PlaylistUpdatedEvent }           from './events';
 import { RemoveTrackFromPlaylistAction }  from './actions';
 import { UserConnectedEvent }             from './events';
 import { UserDisconnectedEvent }          from './events';
+import express                            from 'express';
 import Playlist                           from './models/playlist';
 import SocketIO                           from 'socket.io';
 import Track                              from './models/track';
 import User                               from './models/user';
 
 
-class Server {
-  private sockets: SocketIO.Server;
-  private users: Map<string, User>;
-  private playlist: Playlist;
+export default function configureServer(): http.Server {
+  const app = express();
+  const httpServer = http.createServer(app);
+  const sockets = SocketIO(httpServer) as SocketIO.Server;
+  const users = new Map<string, User>();
+  const playlist = new Playlist();
 
-  constructor(httpServer: http.Server) {
-    this.sockets = SocketIO(httpServer) as SocketIO.Server;
-    this.users = new Map<string, User>();
-    this.playlist = new Playlist();
-
-    this.sockets.on('connection', socket => {
-      const user = this.connectUser(socket);
-      socket.on('disconnect', () => {
-        this.disconnectUser(socket);
-        this.sockets.emit(UserDisconnectedEvent.eventName, new UserDisconnectedEvent(user));
-      });
-
-      socket.on(PlaylistRequestedEvent.eventName, () => {
-        socket.emit(PlaylistRequestedEvent.eventName, new PlaylistRequestedEvent(this.playlist));
-      });
-
-      socket.on(AddTrackToPlaylistAction.actionName, ({ trackID }, ackFn) => {
-        const track = new Track(trackID, user.userID);
-        this.playlist.add(track);
-        if (ackFn)
-          ackFn(true);
-
-        socket.emit(PlaylistUpdatedEvent.eventName, new PlaylistUpdatedEvent(this.playlist));
-      });
-
-      socket.on(RemoveTrackFromPlaylistAction.actionName, ({ trackID }, ackFn) => {
-        const track = new Track(trackID, user.userID);
-        this.playlist.remove(track);
-
-        if (ackFn)
-          ackFn(true);
-
-        socket.emit(PlaylistUpdatedEvent.eventName, new PlaylistUpdatedEvent(this.playlist));
-      });
-
-      socket.on(ClearPlaylist.actionName, ackFn => {
-        this.playlist.clear();
-
-        if (ackFn)
-          ackFn(true);
-
-        socket.emit(PlaylistUpdatedEvent.eventName, new PlaylistUpdatedEvent(this.playlist));
-      });
-      const event = new UserConnectedEvent(user);
-      this.sockets.emit(UserConnectedEvent.eventName, event);
-    });
-  }
-
-  private connectUser(socket: SocketIO.Socket): User {
+  function connectUser(socket: SocketIO.Socket): User {
     const user = new User(socket);
-    this.users.set(socket.id, user);
+    users.set(socket.id, user);
     return user;
   }
 
-  private disconnectUser(socket: SocketIO.Socket): void {
-    this.users.delete(socket.id);
+  function disconnectUser(socket: SocketIO.Socket): void {
+    users.delete(socket.id);
   }
-}
 
-export default Server;
+  sockets.on('connection', socket => {
+    const user = connectUser(socket);
+    socket.on('disconnect', () => {
+      disconnectUser(socket);
+      sockets.emit(UserDisconnectedEvent.eventName, new UserDisconnectedEvent(user));
+    });
+
+    socket.on(PlaylistRequestedEvent.eventName, () => {
+      socket.emit(PlaylistRequestedEvent.eventName, new PlaylistRequestedEvent(playlist));
+    });
+
+    socket.on(AddTrackToPlaylistAction.actionName, ({ trackID }, ackFn) => {
+      const track = new Track(trackID, user.userID);
+      playlist.add(track);
+      if (ackFn)
+        ackFn(true);
+
+      socket.emit(PlaylistUpdatedEvent.eventName, new PlaylistUpdatedEvent(playlist));
+    });
+
+    socket.on(RemoveTrackFromPlaylistAction.actionName, ({ trackID }, ackFn) => {
+      const track = new Track(trackID, user.userID);
+      playlist.remove(track);
+
+      if (ackFn)
+        ackFn(true);
+
+      socket.emit(PlaylistUpdatedEvent.eventName, new PlaylistUpdatedEvent(playlist));
+    });
+
+    socket.on(ClearPlaylist.actionName, ackFn => {
+      playlist.clear();
+
+      if (ackFn)
+        ackFn(true);
+
+      socket.emit(PlaylistUpdatedEvent.eventName, new PlaylistUpdatedEvent(playlist));
+    });
+
+    const event = new UserConnectedEvent(user);
+    sockets.emit(UserConnectedEvent.eventName, event);
+  });
+
+  return httpServer;
+
+}
