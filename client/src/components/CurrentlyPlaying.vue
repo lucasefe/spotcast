1d5971d8d880ef6614a41670e8fb207bf9500352
Lucas Florio <lucasefe@gmail.com>
2020-04-03 12:42:57 -0300

    better look an feel for the player.

--- client/src/components/CurrentlyPlaying.vue
+++ client/src/components/CurrentlyPlaying.vue
@@ -1,10 +1,24 @@
 <template>
   <div v-if="item">
-    <p>{{ item.name }}</p>
-    <p>{{ item.album.name}}</p>
-    <p>{{ item.artists[0].name}}</p>
+    <div class="card">
+      <div class="card-image">
+        <figure class="image is-4by3">
+          <img v-bind:src="albumCover().url" v-bind:width="albumCover().width" v-bind:height="albumCover().height">
+        </figure>
+      </div>
+      <div class="card-content">
+        <div class="media">
+          <div class="media-content">
+            <p class="title is-4"> {{ songName() }}</p>
+            <p class="subtitle is-6"> {{ artistName() }}</p>
+          </div>
+        </div>
 
-    <b-progress :value="progress()" type="is-success" size="is-small" format="percent"></b-progress>
+        <div class="content">
+          <b-progress :value="progress()" type="is-success" size="is-small" format="percent"></b-progress>
+        </div>
+      </div>
+    </div>
 
 
   </div>
@@ -14,7 +28,6 @@
   export default {
     sockets: {
       PLAYER_UPDATED: function(socket_data) {
-        console.debug({ socket_data })
         this.item       = socket_data.item;
         this.progressMS = socket_data.progressMS
       }
@@ -29,6 +42,18 @@
       progress: function() {
         const current = this.progressMS *100 / this.item.duration_ms
         return current;
+      },
+      songName: function() {
+        return this.item.name;
+      },
+      albumName: function() {
+        return this.item.album.name;
+      },
+      artistName: function() {
+        return this.item.artists.map(a => a.name).join(", ")
+      },
+      albumCover: function() {
+        return this.item.album.images[0];
       }
     }
   }
