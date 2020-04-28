2cc48e8ecfd3df1cd0e28ae3d038d5e7d1b2b52b
Lucas Florio <lucasefe@gmail.com>
2020-04-28 14:48:41 -0300

    Rollbar enabled only in prod

--- server/lib/rollbar.js
+++ server/lib/rollbar.js
@@ -7,7 +7,7 @@ const rollbar = new Rollbar({
   accessToken: '05e603c0cf2e421f89fee70dfb6a92e4',
   captureUncaught: true,
   captureUnhandledRejections: true,
-
+  enabled: process.env.NODE_ENV === 'production'
 });
 
 module.exports = rollbar
